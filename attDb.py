533b360958a9b8ec6838c05bf375e721113453a6
Fernando <fernandosouza@cc.ci.ufpb.br>
2018-06-13 00:01:58 -0300

    min price added

--- attDb.py
+++ attDb.py
@@ -14,13 +14,17 @@ for json_item in json_content:
     data = r.json().get('results')
 
     if (len(data) == 0):
-        r = requests.post('http://localhost:8000/api/v1/games/', data={'official': json_item['nameNu'],'name': json_item['nameNu'], 'priceNu': float(price), 'linkNu' : json_item['linkNu'], 'img': json_item['imgNu']} )
+        r = requests.post('http://localhost:8000/api/v1/games/', data={'official': json_item['nameNu'],'name': json_item['nameNu'],'price': float(price) ,'priceNu': float(price), 'linkNu' : json_item['linkNu'], 'img': json_item['imgNu']} )
         print("nuuvem game added")
     for item in data:
         if(len(data) != 0):
             nuuvemID = item['id']
             r = requests.put('http://localhost:8000/api/v1/games/'+ str(nuuvemID) + '/',data={'priceNu': float(price), 'linkNu' : json_item['linkNu']})
             print("NEW PRICE WITH nuuvem")
+            if(item['price'] is None or item['price'] > float(price)):
+                r = requests.put('http://localhost:8000/api/v1/games/'+ str(nuuvemID) + '/',data={'price': float(price)})
+                print("new MIN Price WITH NUUVEM")
+
 
 
 ######################### steam data #######################
@@ -35,7 +39,7 @@ for json_item in json_content:
     r = requests.get('http://localhost:8000/api/v1/games/', params=test)
     data = r.json().get('results')
     if (len(data) == 0):
-        r = requests.post('http://localhost:8000/api/v1/games/', data={'official': json_item['nameSteam'],'name': json_item['nameSteam'], 'priceSteam': float(price), 'linkSteam': json_item['linkSteam'], 'img': json_item['imgSteam']})
+        r = requests.post('http://localhost:8000/api/v1/games/', data={'official': json_item['nameSteam'],'name': json_item['nameSteam'], 'price': float(price), 'priceSteam': float(price), 'linkSteam': json_item['linkSteam'], 'img': json_item['imgSteam']})
         print("steam game added")
 
     for item in data:
@@ -43,6 +47,10 @@ for json_item in json_content:
             steamID = item['id']
             r = requests.put('http://localhost:8000/api/v1/games/'+ str(steamID) + '/', data={'priceSteam': float(price), 'linkSteam': json_item['linkSteam']})
             print("NEW PRICE WITH STEAM")
+            if(item['price'] is None or item['price'] > float(price)):
+                r = requests.put('http://localhost:8000/api/v1/games/'+ str(nuuvemID) + '/',data={'price': float(price)})
+                print("new MIN Price WITH STEAM")
+            
 
 
 ######################### G2a data #######################
@@ -58,7 +66,7 @@ for json_item in json_content:
     data = r.json().get('results')
 
     if (len(data) == 0):
-        r = requests.post('http://localhost:8000/api/v1/games/', data={'official': json_item['nameG2a'],'name': json_item['nameG2a'], 'priceG2a': float(price), 'linkG2a' : json_item['linkG2a'], 'img': json_item['imgG2a']} )
+        r = requests.post('http://localhost:8000/api/v1/games/', data={'official': json_item['nameG2a'],'name': json_item['nameG2a'], 'price': float(price), 'priceG2a': float(price), 'linkG2a' : json_item['linkG2a'], 'img': json_item['imgG2a']} )
         print("G2a game added")
 
     for item in data:
@@ -66,3 +74,6 @@ for json_item in json_content:
             G2aID = item['id']
             r = requests.put('http://localhost:8000/api/v1/games/'+ str(G2aID) + '/',data={'priceG2a': float(price), 'linkG2a' : json_item['linkG2a']} )
             print("NEW PRICE WITH G2a")
+            if(item['price'] is None or item['price'] > float(price)):
+                r = requests.put('http://localhost:8000/api/v1/games/'+ str(nuuvemID) + '/',data={'price': float(price)})
+                print("new MIN Price WITH G2A")
